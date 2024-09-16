58322e7de17aca9cdd999ee0c004e24eef6ce0d4
cutm <debendra.maharana@cutm.ac.in>
2024-09-16 15:25:48 +0530

    Rashid program spring boot

---
+++ src/main/java/com/Sringbootproject1/SpringbootProject1Application.java
@@ -0,0 +1,14 @@
+package com.Sringbootproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProject1Application.class, args);
+		System.out.println("Rashid");
+	}
+
+}
